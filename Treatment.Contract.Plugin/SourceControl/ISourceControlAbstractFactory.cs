f29b6bd2dbe6a78e92bc98cc049ad54fb09401f5
Coen Munckhof <c.munckhof@gmail.com>
2018-05-01 23:02:50 +0200

    Added (source control) contract for plugins

---
+++ Treatment.Contract.Plugin/SourceControl/ISourceControlAbstractFactory.cs
@@ -0,0 +1,13 @@
+﻿namespace Treatment.Contract.Plugin.SourceControl
+{
+    public interface ISourceControlAbstractFactory
+    {
+        string Name { get; }
+
+        int Priority { get; }
+
+        IReadOnlySourceControl Create();
+
+        bool CanCreate(string name);
+    }
+}
