ebaf2561610f43214be55dbbd50aaa848cb855b5
Coen Munckhof <c.munckhof@gmail.com>
2018-10-08 22:13:23 +0200

    Fix merge

--- src/Treatment.UI/DelayCommandExecution.cs
+++ src/Treatment.UI/DelayCommandExecution.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Diagnostics;
+    using System.Threading;
     using System.Threading.Tasks;
 
     using JetBrains.Annotations;
@@ -41,17 +42,19 @@
             }
 
             [DebuggerStepThrough]
-            public async Task ExecuteAsync(TCommand command)
+            public async Task ExecuteAsync(TCommand command, IProgress<ProgressData> progress = null, CancellationToken ct = default(CancellationToken))
             {
-                await _delayService.DelayAsync(command).ConfigureAwait(false);
+                progress?.Report(new ProgressData("Intentionally delay the execution of the command."));
+                await _delayService.DelayAsync(command, ct).ConfigureAwait(false);
+                progress?.Report(new ProgressData("Delayed the execution enough.."));
 
-                await _decoratee.ExecuteAsync(command).ConfigureAwait(false);
+                await _decoratee.ExecuteAsync(command, progress, ct).ConfigureAwait(false);
             }
         }
 
         private interface IDelayService
         {
-            Task DelayAsync<TCommand>([NotNull] TCommand command) where TCommand : ICommand;
+            Task DelayAsync<TCommand>([NotNull] TCommand command, CancellationToken ct = default(CancellationToken)) where TCommand : ICommand;
         }
 
         private class RandomDelayService : IDelayService
@@ -67,10 +70,10 @@
                 _random = new Random();
             }
 
-            public async Task DelayAsync<TCommand>(TCommand command) where TCommand : ICommand
+            public async Task DelayAsync<TCommand>(TCommand command, CancellationToken ct = default(CancellationToken)) where TCommand : ICommand
             {
                 var millisecondsDelay = _random.Next(_minMilliseconds, _maxMilliseconds);
-                await Task.Delay(millisecondsDelay).ConfigureAwait(false);
+                await Task.Delay(millisecondsDelay, ct).ConfigureAwait(false);
             }
         }
     }
