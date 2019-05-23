0e7010b069aae15393a43681f8d3656a27cc03d3
Coen Munckhof <c.munckhof@gmail.com>
2019-05-23 23:59:15 +0200

    Add extra versioning information.

---
+++ scripts/Config.ps1
@@ -0,0 +1,7 @@
+$root_dir = ".."
+
+$git_version = "C:\ProgramData\chocolatey\bin\GitVersion.exe"
+
+$git_version_file = "$root_dir\version.json"
+
+$output_filename = "$root_dir\src\GitVersionInformation.generated.cs"
---
+++ scripts/PreBuild.ps1
@@ -0,0 +1,13 @@
+# Build script that is called when building the software in visual studio.
+# More specifically, this is called before starting the build.
+
+# Set the script folder as the current working directory.
+$scriptpath = $MyInvocation.MyCommand.Path
+Set-Location -Path (Split-Path $scriptpath) 
+
+# Include the relevant modules.
+. .\Config.ps1
+. .\GenerateVersionFile.ps1
+
+Generate-Version-File $output_filename $git_version_file
+
