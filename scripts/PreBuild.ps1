2f3c7c592e673fdbcfe342498bb8e9f828d776e7
Coen van den Munckhof <c.munckhof@gmail.com>
2019-05-24 12:01:40 +0200

    add logging to solve path problem.

--- scripts/PreBuild.ps1
+++ scripts/PreBuild.ps1
@@ -3,7 +3,25 @@
 
 # Set the script folder as the current working directory.
 $scriptpath = $MyInvocation.MyCommand.Path
+
+# Save the current location.
+$CurrentDir = $(Get-Location).Path;
+Write-Host "CurrentDir: " $CurrentDir
+
+# Get location of powershell file
+Write-Host "PSScriptRoot: " $PSScriptRoot
+
+# we know this script is located in the .scripts\ folder of the root.
+$RootDir = [IO.Path]::GetFullPath( (join-path $PSScriptRoot "..\") )
+Write-Host "ROOT: " $RootDir
+
+
+Write-Host "scriptpath: $scriptpath"
 Set-Location -Path (Split-Path $scriptpath) 
+
+
+
+
 
 # Include the relevant modules.
 . .\Config.ps1
@@ -11,3 +29,5 @@ Set-Location -Path (Split-Path $scriptpath)
 
 Generate-Version-File $output_filename $git_version_file
 
+$cd = $(Get-Location).Path;
+Write-Host "current dir: $cd"
