2029fbca8040ff93e043f020ff1e325aa7ef90fb
Coen van den Munckhof <c.munckhof@gmail.com>
2019-05-24 12:08:56 +0200

    Fix appveyor bulid

--- scripts/Config.ps1
+++ scripts/Config.ps1
@@ -1,4 +1,7 @@
-$root_dir = ".."
+# we know this script is located in the .scripts\ folder of the root.
+$root_dir = [IO.Path]::GetFullPath( (join-path $PSScriptRoot "..\") )
+Write-Host "Root directory : " $root_dir
+
 
 $git_version = $env:gitversion_executable
 If ($git_version -eq $null -or ( Test-Path $git_version ) -eq $false )
@@ -6,6 +9,7 @@ If ($git_version -eq $null -or ( Test-Path $git_version ) -eq $false )
 	Write-Host "Set GitVersion to fixed path."
 	$git_version = "C:\ProgramData\chocolatey\bin\GitVersion.exe"
 }
+
 
 $git_version_file = "$root_dir\version.json"
 	
--- scripts/PreBuild.ps1
+++ scripts/PreBuild.ps1
@@ -6,28 +6,18 @@ $scriptpath = $MyInvocation.MyCommand.Path
 
 # Save the current location.
 $CurrentDir = $(Get-Location).Path;
-Write-Host "CurrentDir: " $CurrentDir
-
-# Get location of powershell file
-Write-Host "PSScriptRoot: " $PSScriptRoot
-
-# we know this script is located in the .scripts\ folder of the root.
-$RootDir = [IO.Path]::GetFullPath( (join-path $PSScriptRoot "..\") )
-Write-Host "ROOT: " $RootDir
 
 
-Write-Host "scriptpath: $scriptpath"
-Set-Location -Path (Split-Path $scriptpath) 
+Try
+{
+	Set-Location -Path (Split-Path $scriptpath) 
 
-
-
-
-
-# Include the relevant modules.
-. .\Config.ps1
-. .\GenerateVersionFile.ps1
-
-Generate-Version-File $output_filename $git_version_file
-
-$cd = $(Get-Location).Path;
-Write-Host "current dir: $cd"
+	# Include the relevant modules.
+	. .\Config.ps1
+	. .\GenerateVersionFile.ps1
+	Generate-Version-File $output_filename $git_version_file
+}
+Finally
+{
+	cd $CurrentDir
+}
