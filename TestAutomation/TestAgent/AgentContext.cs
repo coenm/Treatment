3038dc64feaaed8cb9bb09b2dcfb7d859f95cd38
Coen van den Munckhof <c.munckhof@gmail.com>
2019-05-29 23:01:20 +0200

    stylecopping

--- TestAutomation/TestAgent/AgentContext.cs
+++ TestAutomation/TestAgent/AgentContext.cs
@@ -2,15 +2,11 @@
 {
     using System.Threading;
     using System.Threading.Tasks;
-    using System.Windows.Navigation;
 
     using JetBrains.Annotations;
     using Treatment.Helpers.Guards;
-
-    using TreatmentZeroMq.ContextService;
     using TreatmentZeroMq.Helpers;
     using TreatmentZeroMq.Socket;
-
     using ZeroMQ;
 
     public class AgentContext : IAgentContext
@@ -34,7 +30,12 @@
             Guard.NotNull(command, nameof(command));
             this.command = command;
 
-            Task.Run(async () => await StartMonitoring(command));
+            Task.Run(async () => await StartMonitoring(command), CancellationToken.None);
+        }
+
+        public void Stop()
+        {
+            cancellationTokenSource.Cancel();
         }
 
         private async Task StartMonitoring(Medallion.Shell.Command command)
@@ -43,23 +44,19 @@
 
             using (var socket = socketFactory.Create(ZSocketType.PUB))
             {
-                socket.TryConnect("inproc://publish");
-
-                socket.Send(new ZMessage(new[]
-                                                     {
-                                                         new ZFrame("AGENT"),
-                                                         new ZFrame("SUT PROCESS"),
-                                                         new ZFrame(result.StandardOutput),
-                                                         new ZFrame(result.StandardError),
-                                                         new ZFrame(result.ExitCode),
-                                                         new ZFrame(result.Success ? "1" : "0"),
-                                                     }));
+                if (socket.TryConnect("inproc://publish"))
+                {
+                    socket.Send(new ZMessage(new[]
+                                             {
+                                                 new ZFrame("AGENT"),
+                                                 new ZFrame("SUT PROCESS"),
+                                                 new ZFrame(result.StandardOutput),
+                                                 new ZFrame(result.StandardError),
+                                                 new ZFrame(result.ExitCode),
+                                                 new ZFrame(result.Success ? "1" : "0"),
+                                             }));
+                }
             }
-        }
-
-        public void Stop()
-        {
-            cancellationTokenSource.Cancel();
         }
     }
 }
