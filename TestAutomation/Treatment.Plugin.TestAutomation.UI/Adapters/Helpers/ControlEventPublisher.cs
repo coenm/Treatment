61c2e76448bddfd7480e07a6f025965ddbc68cf6
Coen Munckhof <c.munckhof@gmail.com>
2019-07-03 20:14:54 +0200

    Moved ControlEventPublisher to new folder

--- TestAutomation/Treatment.Plugin.TestAutomation.UI/Adapters/Helpers/ControlEventPublisher.cs
+++ TestAutomation/Treatment.Plugin.TestAutomation.UI/Adapters/Helpers/ControlEventPublisher.cs
@@ -1,16 +1,17 @@
-﻿namespace Treatment.Plugin.TestAutomation.UI.Adapters
+﻿namespace Treatment.Plugin.TestAutomation.UI.Adapters.Helpers
 {
     using System;
     using System.Collections.Generic;
     using System.Reflection;
 
     using JetBrains.Annotations;
+
     using Treatment.Helpers.Guards;
     using Treatment.Plugin.TestAutomation.UI.Infrastructure;
     using Treatment.TestAutomation.Contract.Interfaces.Events;
     using Treatment.TestAutomation.Contract.Interfaces.Framework;
 
-    public class ControlEventPublisher : IDisposable
+    internal class ControlEventPublisher : IDisposable
     {
         private readonly Guid guid;
         private readonly IEventPublisher eventPublisher;
