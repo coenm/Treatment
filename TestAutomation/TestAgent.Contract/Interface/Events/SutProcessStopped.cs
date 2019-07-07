be3b334dfa367e207842299e34820ed56b4061e4
Coen Munckhof <c.munckhof@gmail.com>
2019-07-07 22:35:12 +0200

    Added SutProcessStopped event

---
+++ TestAutomation/TestAgent.Contract/Interface/Events/SutProcessStopped.cs
@@ -0,0 +1,16 @@
+﻿namespace TestAgent.Contract.Interface.Events
+{
+    using JetBrains.Annotations;
+
+    [PublicAPI]
+    public class SutProcessStopped : TestAgentEventBase
+    {
+        public bool Success { get; set; }
+
+        public string StandardOutput { get; set; }
+
+        public string StandardError { get; set; }
+
+        public int ExitCode { get; set; }
+    }
+}
